412aadd471169ef16bd75113c806d71eeb24da78
王军伟 <wangjunwei87@gmail.com>
2017-06-26 19:43:02 +0800

    fix typo in file name

--- motan-extension/protocol-extension/motan-protocol-restful/src/test/java/com/weibo/api/motan/protocol/restful/TestServletContainerRestful.java
+++ motan-extension/protocol-extension/motan-protocol-restful/src/test/java/com/weibo/api/motan/protocol/restful/TestServletContainerRestful.java
@@ -33,7 +33,7 @@ import java.util.Map;
  * @author zhouhaocheng
  *
  */
-public class TestServletCotainerRestful extends TestRestful{
+public class TestServletContainerRestful extends TestRestful {
 	private Tomcat tomcat;
 
 	private final String contextpath = "/cp";
